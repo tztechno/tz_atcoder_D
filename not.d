d55343e809f5cdbded98a9fa1bffe072eb50fcd0
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-03 20:25:53 +0900

    Create not.d

---
+++ not.d
@@ -0,0 +1,14 @@
+//ABC178_A Not
+
+import std.stdio;
+import std.string;
+
+void main()
+{
+  string S = readln().chomp();
+  if (S=="0"){
+    writeln(1);
+  } else {
+    writeln(0);      
+  }
+}
