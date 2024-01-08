b1c8c8256e5eea808fd70a014694d5bba9405459
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-08 18:54:53 +0900

    Create late.d

---
+++ late.d
@@ -0,0 +1,14 @@
+//ABC177_A late
+
+import std.stdio;
+
+void main()
+{
+int D,T,S;
+readf("%d %d %d", &D, &T, &S);
+  if (D<=T*S){
+    writeln("Yes");
+  } else {
+    writeln("No");      
+  }
+}
